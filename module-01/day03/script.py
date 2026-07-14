f2676dc7785781534ee1de4d06572eea2f820ada
eyerus-megersa <megersaeyerus11@gmail.com>
2026-07-14 04:43:40 -0700

    day 03 ex

---
+++ module-01/day03/script.py
@@ -0,0 +1,38 @@
+
+# transaction loge reader
+try:
+ with open("transaction.txt","r") as file:
+    transaction_dic = {}
+    
+    for line in file:
+      name,total_spend = line.strip().split("=")
+      total_spend = float(total_spend)
+
+      if name in transaction_dic:
+        transaction_dic[name]+= total_spend
+      else:
+        transaction_dic[name] =total_spend
+
+except FileNotFoundError:
+  print("file not founde.")
+  
+
+def sort_transaction(transaction_dic):
+
+    sort_transaction=sorted(transaction_dic.items(),
+    key=lambda item: item[1],
+    reverse=true
+    
+    ),
+
+
+with open("report.txt", "w") as file:
+      file.write("Customer Report \n\n\n")
+
+      for name,total_spend in transaction_dic.items():
+        file.write(f"{name}:{total_spend}\n\n")
+
+      file.write(f"customer length = {len(name)}")
+
+
+
